39a9bf59df0769df0a197c75c1ed4f9275cd4f97
macbook <lawaltoheeb36@gmail.com>
2026-06-20 03:37:41 +0100

    batch tx signing

--- engine-bridge/src/index.ts
+++ engine-bridge/src/index.ts
@@ -1,8 +1,10 @@
 export { RpcClient }             from "./rpc-client";
 export { NonceManager }          from "./nonce-manager";
+export { TxAggregator }          from "./tx-aggregator";
 export { EventPropagator }       from "./event-propagator";
 export type { EngineEvent }      from "./event-propagator";
 export { ZkStateSyncer }         from "./zk-state-syncer";
 export type { ZkStateSnapshot, ZkStateSyncerOptions } from "./zk-state-syncer";
 export { RelayerAuth }           from "./relayer-auth";
 export type { RelayerAuthOptions, VerifyClientInfo, VerifyClientCallback, AuthResult } from "./relayer-auth";
+export type { BatchFeePlan, BuildBatchTransactionOptions, BuiltBatchTransaction } from "./tx-aggregator";
